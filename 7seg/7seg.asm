62105994d5e683e7693045131b2dcc388759fce8
Martin <73581070+clairvoyant-xo@users.noreply.github.com>
2023-09-20 04:03:19 -0300

    Update 7seg.asm

--- 7seg/7seg.asm
+++ 7seg/7seg.asm
@@ -4,19 +4,16 @@ segmentos_decimales:
 .db 0x3F,0x06,0x5B,0x4F,0x66
 .db 0x6D,0x7D,0x08,0x7F,0x6F
 
-; Vector de reset
 .org 0
 		rjmp Reset 
-; Vector de INT0 (pin PD2) 
+
 .org INT0addr
 		rjmp IntV0 
 
-; Vector de PCINT0 (pines PB0 y PB1, en este caso)
 .org PCI0addr
 		rjmp IntVC0    
 
-; Inicializa stack
-Reset:	ldi R31,low(RAMEND) 
+reset:	ldi R31,low(RAMEND) 
 	  	out SPL,R31
         ldi R31, high(RAMEND)
         out SPH, R31
@@ -24,78 +21,82 @@ Reset:	ldi R31,low(RAMEND)
 		call INIT_IRQ_INT0
 		call INIT_IRQ_PIN_CHANGE
 					
- 		clr R16							
-		out DDRD, R17 ; PORTD como entrada
-		out DDRB, R17 ; PORTB como entrada
-		sei	          ; Hab. global de interrupciones
+ 		ldi r16,0xFB						
+		out DDRD, R16
+		ldi r16, 0x04
+		out DDRB, R16
+		sei	          
 
 main:
-    ldi r0,0
+    ldi r16,0
+	mov r0,r16
     call actualizar_7segmentos
     loop:
         rjmp loop      
 
 setear_0:
-    ldi r0,0xFF
+    ldi r16,0xFF
+	mov r0,r16
     ret
 
 setear_9:
-    ldi r0,9
+    ldi r16,10
+	mov r0,r16
     ret
 
 IntV0:  
 		
-        push r16 ;****
+        push r16 
 		
 		in   r16,sreg
-		push r16 ;====
+		push r16 
 		
         cpi r0,9
         breq setear_0
         inc r0
         call actualizar_7segmentos
 		
-		pop r16 ;=====
+		pop r16 
 		out sreg,r16
 
-		pop r16 ;****
+		pop r16 
 		reti
 
 IntVC0:	
 
-        push r16 ;****
+        push r16 
 		
 		in   r16,sreg
-		push r16 ;====
+		push r16 
 		
+		cpi r0,0
+        breq setear_9
         dec r0
         call actualizar_7segmentos
 		
-		pop r16 ;=====
+		pop r16 
 		out sreg,r16
 
-		pop r16 ;****
+		pop r16 
 		reti
 
-; Configura PCINT0, PCINT1
-INIT_IRQ_PIN_CHANGE:	;0b00000001
+INIT_IRQ_PIN_CHANGE:	
 		ldi R16, (1<<PCIE0) 				
 		STS PCICR, R16
 		ldi R16, (1 << PCINT0) | (1 << PCINT1)
 		STS PCMSK0, R16 
 		RET
 
-; Configura INT0
 INIT_IRQ_INT0:
-		ldi R16, (1<<ISC01)|(0<<ISC00)  ;flanco descendente	
+		ldi R16, (1<<ISC01)|(0<<ISC00)  
 		sts EICRA, R16					
 		ldi R16, (1<<INT0)				
-		out EIMSK, R16					; Habilita máscara
+		out EIMSK, R16					
 		RET    
 
 ; actualizar_7segmentos:
 ; Muestra un dígito en un display de 7 segmentos
-; (dp,g,f,e,d,c,b,a) = (PD7,PD6,PB5,PB4,PB3,PB2,PB1,PB0)
+; (dp,g,f,e,d,c,b,a) = (PD7,PD6,PD5,PD4,PD3,PB2,PD1,PD0)
 ; Entrada: r0 = digito decimal a mostrar en el display
 ; Salida: pines de salida del display actualizados
 ;---------------------------------------
@@ -106,8 +107,13 @@ actualizar_7segmentos:
     brcc no_incrementar_zh
     inc zh
 no_incrementar_zh:      
-    lpm r0, z
-    out portb, r0
-    andi r0, 0b11100000
-    out portd, r0
+    lpm r16, z
+	mov r17, r16
+	andi r17, 0b11111011
+	ori r17, 0b00000100
+    out portd, r17
+	mov r17, r16
+    andi r17, 0b00000100
+	ori r17, 0b00000011
+    out portb, r17
     ret
